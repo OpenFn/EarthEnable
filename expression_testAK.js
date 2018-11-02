c108ebe75834eff0b7cdde53447f8be55eaefb52
Taylor Downs <downs.taylor@gmail.com>
2018-11-02 11:53:30 +0000

    refactor

--- expression_testAK.js
+++ expression_testAK.js
@@ -1,11 +1,16 @@
 alterState(state => {
+	// prefer object destructuring...
+	const { msisdn, District, reference_number, house_number, contract_number, payment_number } = state.data;
+
 	console.log("Aleksa is testing...");
-  	const msisdn = (state.data.msisdn.substr(0,3) == "250" ? 'RW' : 'Msisdn code not found' );
-    console.log(msisdn);
+  	const countryCode = (msisdn.substr(0,3) == "250" ? 'RW' : 'Msisdn code not found' );
+    console.log(countryCode);
 
-
-    function findDistrict(dis) {
-    	const districtFIND = {
+		// =========================================================================
+		// TODO: Aleksa discuss with TD. Why are you catching this error?
+		// What will happen when we send 'District code not found' downstream?
+    function findDistrictCode(district) {
+    	const districtFind = {
     		Bugesera : 'BT',
     		Gatsibo : 'GT',
     		Kamonyi : 'KM',
@@ -13,27 +18,28 @@ alterState(state => {
     		Ngoma : 'NG',
     		Rwamagana : 'RW'
     	};
-    	return districtFIND[dis] || 'District code not found';
+    	return districtFind[district] || 'District code not found';
     }
-    const district = findDistrict(state.data.District);
-    console.log(district);
-    
+		// =========================================================================
+
+    const dist = findDistrictCode(District);
+    console.log(dist);
 
     const date = new Date().getFullYear().toString().substr(-2);
     console.log(date);
 
+		const houseId = `${countryCode}${District}${date}-${reference_number}-${house_number}`;
 
-    console.log("Here is the concatenated Payment Id...");
-    const paymentId = msisdn.concat(district,date,"-",state.data.reference_number,"-",state.data.house_number,"-",state.data.contract_number,"-",state.data.payment_number)
-    console.log(paymentId);
-    // generates "RWBT18-8383-8273-7274-8283" with the first payload EE shared in their email
 
-    console.log("And here is the concatenated Contract Id without payment number appended...");
-    const contractId = msisdn.concat(district,date,"-",state.data.reference_number,"-",state.data.house_number,"-",state.data.contract_number)
+    console.log('And here is the concatenated Contract Id without payment number appended...');
+    const contractId = `${houseId}-${contract_number}`;
     console.log(contractId);
     // generates "RWBT18-8383-8273-7274" with the first payload EE shared in their email
 
-  	return state;
+		console.log('Here is the concatenated Payment Id...');
+		const paymentId = `${contractId}-${payment_number}`;
+		console.log(paymentId);
+		// generates "RWBT18-8383-8273-7274-8283" with the first payload EE shared in their email
 
+  	return state;
 });
- 
