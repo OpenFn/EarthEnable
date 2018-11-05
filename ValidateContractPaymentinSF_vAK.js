13770f595db0c06175cf0c6e50071879ec43414f
Taylor Downs <downs.taylor@gmail.com>
2018-11-05 13:21:47 +0200

    let dist be undefined

--- ValidateContractPaymentinSF_vAK.js
+++ ValidateContractPaymentinSF_vAK.js
@@ -1,25 +1,18 @@
 upsert('Payment__c', 'EE_Payment_ID__c', fields(
   field("EE_Payment_ID__c", state => {
   		const { msisdn, District, reference_number, house_number, contract_number, payment_number } = state.data;
+      const districts = {
+        Bugesera: 'BT',
+        Gatsibo: 'GT',
+        Kamonyi: 'KM',
+        Kayonza: 'KY',
+        Ngoma: 'NG',
+        Rwamagana: 'RW'
+      };
 
   		const countryCode = (msisdn.substr(0,3) == "250" ? 'RW' : 'Msisdn code not found' );
-		// =========================================================================
-		// TODO: Aleksa discuss with TD. Why are you catching this error?
-		// What will happen when we send 'District code not found' downstream?
-   		function findDistrictCode(district) {
-	    	const districtFind = {
-	    		Bugesera : 'BT',
-	    		Gatsibo : 'GT',
-	    		Kamonyi : 'KM',
-	    		Kayonza : 'KY',
-	    		Ngoma : 'NG',
-	    		Rwamagana : 'RW'
-	    	};
-    	return districtFind[district] || 'District code not found';
-    	}
-		// =========================================================================
 
-	    const dist = findDistrictCode(District);
+	    const dist = districts[District];
 
 	    const date = new Date().getFullYear().toString().substr(-2);
 
@@ -59,7 +52,7 @@ upsert('Payment__c', 'EE_Payment_ID__c', fields(
 		  const houseId = `${countryCode}${District}${date}-${reference_number}-${house_number}`;
 
 		  const contractId = `${houseId}-${contract_number}`;
-  
+
 		    return contractId;
   	}),
   field("Amount_of_Payment__c", dataValue("amount")),
