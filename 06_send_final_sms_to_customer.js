3f6012da06fba036096bf2eab6d5560b929dd775
Taylor Downs <downs.taylor@gmail.com>
2018-11-14 15:59:47 +0000

    ok

--- 06_send_final_sms_to_customer.js
+++ 06_send_final_sms_to_customer.js
@@ -6,7 +6,7 @@ alterState(state =>{
 get('/sendsms/earthenable', {
   query: {
     api_token: state.configuration.password,
-    receiver: parseInt(state.references[1].Envelope.Body.notifications.Notification.sObject.MSISDN__c),
+    receiver: parseInt(state.references[2].Envelope.Body.notifications.Notification.sObject.MSISDN__c),
     message: `Earthenable has received your payment of ${state.contractData.Total_Amount_Paid__c}. To begin construction you have a remainder of ${state.contractData.Amount_to_left_to_pay_pre_sand_delivery__c} to pay. To complete your contract you must pay ${state.contractData.Total_Amount_Left_to_Pay__c}.`,
   }
 });
