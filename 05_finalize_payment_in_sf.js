395ff855ef99ca78a143d3b5224ae2ae703273ec
Taylor Downs <downs.taylor@gmail.com>
2018-11-14 15:38:02 +0000

    more

--- 05_finalize_payment_in_sf.js
+++ 05_finalize_payment_in_sf.js
@@ -1,39 +1,16 @@
-// alterState(state => {
-//   console.log(state);
-//   state.districts = {
-//     Bugesera: 'BG',
-//     Gatsibo: 'GT',
-//     Kamonyi: 'KM',
-//     Kayonza: 'KY',
-//     Ngoma: 'NG',
-//     Rwamagana: 'RW'
-//   };
-//
-//   const {
-//     EE_Payment_ID__c
-//   } = state.data.Envelope.Body.notifications.Notification.sObject;
-//
-//   const country = (msisdn.substr(0, 3) == "250" ? 'RW' : 'Msisdn code not found');
-//   const dist = state.districts[District];
-//   const date = new Date().getFullYear().toString().substr(-2);
-//   const houseId = `${country}${dist}${date}-${reference_number}-${house_number}`;
-//   state.contractId = `${houseId}-${contract_number}`;
-//   state.paymentId = `${state.contractId}-${payment_number}`;
-//
-//   return state;
-// });
-
-alterState(state => {
-  console.log(state.references[0].Envelope.Body.notifications.Notification.sObject);
-  return state;
-});
-
 steps(
+  // First update the paymnent status.
   upsert('Payment__c', 'EE_Payment_ID__c', fields(
     field("EE_Payment_ID__c", state.references[0].Envelope.Body.notifications.Notification.sObject.EE_Payment_ID__c),
     field("Payment_status__c", state.data.transactionStatus)
   )),
   query(
+    // Then fetch the new customer data to send in SMS.
     `SELECT Total_Amount_Paid__c, Amount_to_left_to_pay_pre_sand_delivery__c, Total_Amount_Left_to_Pay__c FROM Phase_Contract__c WHERE EE_Contract_ID__c = '${state.contractId}'`
   )
 );
+
+alterState(state => {
+  state.contractData = state.references[0].records[0]
+  return state;
+});
--- 06_send_final_sms_to_customer.js
+++ 06_send_final_sms_to_customer.js
@@ -1,19 +1,12 @@
 alterState(state =>{
   console.log(state);
-  // we will need to format a nice message for the customer here, based on the
-  // earlier salesforce data.
   return state;
 });
 
-// Your job goes here.
-// when customer data comes from SF, update customer/agent.
-// Thank you you’re payment of [Amount_of_Payment__c] (from payment object).
-// To begin your floor construction you have a remainder of [Amount_to_left_to_pay_pre_sand_delivery__c] to pay.
-// To fully complete your contract you must pay [Total_Amount_Left_to_Pay__c]. (from phase/contract object)
 get('/sendsms/earthenable', {
   query: {
     api_token: state.configuration.password,
-    receiver: 250722878433,
-    message: state.messageText,
+    receiver: parseInt(state.data.msisdn),
+    message: `Earthenable has received your payment of ${state.contractData.Total_Amount_Paid__c}. To begin construction you have a remainder of ${state.contractData.Amount_to_left_to_pay_pre_sand_delivery__c} to pay. To complete your contract you must pay ${state.contractData.Total_Amount_Left_to_Pay__c}.`,
   }
 });
