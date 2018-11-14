195a8f0b6edd19334880d6d47654abcf595915b1
Taylor Downs <downs.taylor@gmail.com>
2018-11-14 15:03:23 +0000

    wrapped

--- 01_validate_payment.js
+++ 01_validate_payment.js
@@ -1,4 +1,4 @@
-alterState(state => {
+state => {
   const {
     msisdn,
     District,
@@ -26,12 +26,12 @@ alterState(state => {
   state.paymentId = `${state.contractId}-${payment_number}`;
 
   return state;
-})
 
-upsert('Payment__c', 'EE_Payment_ID__c', fields(
-  field("EE_Payment_ID__c", state.paymentId),
-  relationship("Associated_Phase_Contract__r", "EE_Contract_ID__c", state.contractId),
-  field("Amount_of_Payment__c", dataValue("amount")),
-  field("Payment_status__c", "INITIATED"),
-  field("Date_Payment_Received_in_Bank__c", Date.now())
-));
+  upsert('Payment__c', 'EE_Payment_ID__c', fields(
+    field("EE_Payment_ID__c", state.paymentId),
+    relationship("Associated_Phase_Contract__r", "EE_Contract_ID__c", state.contractId),
+    field("Amount_of_Payment__c", dataValue("amount")),
+    field("Payment_status__c", "INITIATED"),
+    field("Date_Payment_Received_in_Bank__c", Date.now())
+  ))(state);
+}
