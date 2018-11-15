6bb2307430b5ab103bcfe4a6587ee723d824d6b5
Taylor Downs <downs.taylor@gmail.com>
2018-11-15 12:04:32 +0000

    cleaning #6

--- 06_send_final_sms_to_customer.js
+++ 06_send_final_sms_to_customer.js
@@ -5,13 +5,13 @@ alterState(state => {
     state.paymentSuccess ?
     `Earthenable has received your payment of ${state.sfData.Amount_of_Payment__c}. To begin construction you have a remainder of ${state.contractData.Amount_to_left_to_pay_pre_sand_delivery__c} to pay. To complete your contract you must pay ${state.contractData.Total_Amount_Left_to_Pay__c}.` :
     `Your payment of ${state.sfData.Amount_of_Payment__c} was not able to be completed and your account has not been charged. Please try again.`
-  )
-  console.log('Attemtping to send message: ' + state.messageText);
+  );
+  console.log('Attempting to send message: ' + state.messageText);
 
   return get('/sendsms/earthenable', {
     query: {
       api_token: state.configuration.password,
-      receiver: parseInt(state.references[2].Envelope.Body.notifications.Notification.sObject.Payment_MSISDN__c),
+      receiver: parseInt(state.sfData.Payment_MSISDN__c),
       message: state.messageText,
     }
   })(state);
