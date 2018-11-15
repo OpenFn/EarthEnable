042d6f609f8b121349b06eb42abc07ad54f5bc84
Taylor Downs <downs.taylor@gmail.com>
2018-11-15 11:58:25 +0000

    more

--- 04_check_havanao_status.js
+++ 04_check_havanao_status.js
@@ -1,11 +1,9 @@
-get("https://api.havanao.com/api/sale/status", {
-  query: {
-    "transactionId": state.data.Envelope.Body.notifications.Notification.sObject.EE_Payment_ID__c,
-    "api_token": "KxnJo3KWIs01akwsIq8jRFhs8CKkPffG56nIl0d5QAG2EUcg5NdoKbuaI024"
-  }
-});
-
 alterState(state => {
   state.sfData = state.data.Envelope.Body.notifications.Notification.sObject;
-  return state;
+  return get("https://api.havanao.com/api/sale/status", {
+    query: {
+      "transactionId": state.sfData.EE_Payment_ID__c,
+      "api_token": "KxnJo3KWIs01akwsIq8jRFhs8CKkPffG56nIl0d5QAG2EUcg5NdoKbuaI024"
+    }
+  })(state);
 });
